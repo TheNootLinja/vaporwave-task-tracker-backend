082f2e8e8d3f4bfa0df5d3962cb9f959c6b978f1
TheNootLinja <thenootlinja@gmail.com>
2021-05-15 14:01:52 -0400

    Add comments to admin.js routes page

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,13 @@ const express = require('express');
 const adminController = require('../controllers/admin');
 
 const router = express.Router();
-
+// Get route for home page
 router.get('/', adminController.getIndex);
-
+// Get route for add task page
 router.get('/add-task', adminController.getAddTask);
-
+// Post route for add task page
 router.post('/add-task', adminController.postTask);
-
+// Get route based on a query we pass in the url
 router.get('/:taskId', adminController.getTask);
 
 module.exports = router;
