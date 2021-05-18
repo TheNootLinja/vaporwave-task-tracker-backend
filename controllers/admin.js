01f625890b255952ff4611a6a304fa222e984838
TheNootLinja <thenootlinja@gmail.com>
2021-05-18 19:30:51 -0400

    Fix the api to be able to add a new task from the frontend

--- controllers/admin.js
+++ controllers/admin.js
@@ -69,7 +69,7 @@ exports.getEditTask = async (req, res) => {
 exports.postTask = (req, res) => {
   // destructuring variables and values from the request body
   const { name, dateTime, desc } = req.body;
-
+  console.log(req);
   // setting task variable to an object made of destructured variables
   const task = new Task({ name: name, dateTime: dateTime, desc: desc });
   // adding task to db
